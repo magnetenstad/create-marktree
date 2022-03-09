d1169870878f9788d09f706bf8fbd6ec071ec602
magnetenstad <tenstad.magne@gmail.com>
2022-03-09 20:35:35 +0100

    Edit bin script

--- bin/create-template.js
+++ bin/create-template.js
@@ -4,11 +4,4 @@ import { Directory } from 'virtual-file-system'
 
 const path = process.argv[1].replace('bin\\createTemplate.js', 'template')
 const directory = Directory.read(path, ['node_modules', 'docs'])
-directory.files.forEach((file) => {
-  file.location = ''
-  file.write()
-})
-directory.directories.forEach((directory) => {
-  directory.location = ''
-  directory.write()
-})
+directory.writeContents('')
