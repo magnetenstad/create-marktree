3f061191ad382ce5b49f2295eb56134b4265ec40
magnetenstad <tenstad.magne@gmail.com>
2022-03-10 03:02:32 +0100

    Fix template bin script

--- bin/template.js
+++ bin/template.js
@@ -2,6 +2,6 @@
 
 import { Directory } from 'virtual-file-system'
 
-const path = process.argv[1].replace('bin\\createTemplate.js', 'template')
+const path = process.argv[1].replace('bin\\template.js', 'template')
 const directory = Directory.read(path, ['node_modules', 'docs'])
 directory.writeContents('')
